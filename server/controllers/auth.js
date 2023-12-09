2c684cd816d821e04b2b2db6c352fd92cc0c4587
manu das <manudas1947@gmail.com>
2023-12-09 17:05:14 +0530

    user registration API

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User"
+import User from "../models/User.js"
 
 /* REGISTER USER */
 
@@ -33,6 +33,22 @@ export const register = async (req, res) => {
         })
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
+
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+}
+/*-------LOGGING IN ---------*/
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email })
+        if (!user) return res.status(400).json({ msg: "User Does not exist." });
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials." });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        delete user.password;
+        res.status(200).json({ token, user })
 
     } catch (e) {
         res.status(500).json({ error: e.message })
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
-
+import authRoutes from "./routes/auth.js"
 /* Configurations*/
 
 const __filename = fileURLToPath(import.meta.url);
@@ -43,7 +43,12 @@ const upload = multer({ storage });
 
 app.post("/auth/resiter", upload.single("picture"), register)
 
+/*------- ROUTES ----------*/
+
+app.use("/auth", authRoutes)
+
 /*------- MONGOOSE SETUP --------*/
+
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL).then(
     () => {
